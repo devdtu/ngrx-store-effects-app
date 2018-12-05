e4118b4e8731cc9973389e95ced63f8757594bdb
devdtu <dev.khp@gmail.com>
2018-12-05 18:01:08 +0530

    state and entity composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,12 +1,31 @@
-import { ActionReducerMap } from '@ngrx/store';
+import {
+  ActionReducerMap,
+  createSelector,
+  createFeatureSelector
+} from '@ngrx/store';
 import * as fromPizzas from './pizzas.reducer';
 
-export interface ProductState {
-    pizzas: fromPizzas.PizzaState
+export interface ProductsState {
+  pizzas: fromPizzas.PizzaState;
 }
 
 //register our reducers
-// state is managed by a reducer 
-export const reducers: ActionReducerMap<ProductState> = {
-    pizzas: fromPizzas.reducer
-}
+// state is managed by a reducer
+export const reducers: ActionReducerMap<ProductsState> = {
+  pizzas: fromPizzas.reducer
+};
+
+export const getProductsState = createFeatureSelector<ProductsState>(
+  'products'
+); //
+// this means everything will be nuder 'products'
+
+//pizzas state
+export const getPizzaState = createSelector(
+  getProductsState,
+  (state: ProductsState) => state.pizzas
+);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzaLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzaLoading);
