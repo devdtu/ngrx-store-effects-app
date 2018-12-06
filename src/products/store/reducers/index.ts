8cf12e161fbda7f506797e58a54920f9e504fd31
devdtu <dev.khp@gmail.com>
2018-12-06 14:46:39 +0530

    toppings reducers

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,14 +1,17 @@
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import * as fromPizzas from './pizzas.reducer';
+import * as fromToppings from './toppings.reducer';
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzaState;
+  toppings: fromToppings.ToppingsState;
 }
 
 //register our reducers
 // state is managed by a reducer
 export const reducers: ActionReducerMap<ProductsState> = {
-  pizzas: fromPizzas.reducer
+  pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer
 };
 
 export const getProductsState = createFeatureSelector<ProductsState>(
