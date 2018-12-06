5d84a44e0a8dd4e439efc9f2ddb57d4f5616bb15
devdtu <dev.khp@gmail.com>
2018-12-06 12:53:50 +0530

    custom router state serializers

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -4,7 +4,11 @@ import {
   createSelector,
   ActionReducerMap
 } from '@ngrx/store';
-import { Params } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Params
+} from '@angular/router';
 
 // this defines what the router state will look like
 export interface RouterStateUrl {
@@ -27,5 +31,26 @@ export const getRouterState = createFeatureSelector<
   fromRouter.RouterReducerState<RouterStateUrl>
 >('routerReducer');
 
-// the above statement means  - we are telling createFeatureSelector that you can access 
+// the above statement means  - we are telling createFeatureSelector that you can access
 // this property - "fromRouter.RouterReducerState<RouterStateUrl>" on 'routerReducer'
+
+export class CustomSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  // ****************
+  // If something changes in the url THIS WHOLE FUNCTIONS WILL BE CALLED
+  // ***********
+
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState; // const url = routerState.url
+    const { queryParams } = routerState.root;
+
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      // this is the state tree of angular router
+      // state.firstChild means we have child routes -- /product/1 in url
+      state = state.firstChild;
+    }
+    const { params } = state;
+    return { url, queryParams, params }; // this returned object is What will bound to ngrx store state tree
+  }
+}
