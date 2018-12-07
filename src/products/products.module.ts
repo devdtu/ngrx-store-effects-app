21abc86ec06fc9ec547fc281c6ed3c2cd0de2011
devdtu <dev.khp@gmail.com>
2018-12-07 16:38:57 +0530

    guards that check store entities

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -38,6 +38,8 @@ export const ROUTES: Routes = [
   },
   {
     path: ':pizzaId',
+    
+    canActivate: [fromGuards.PizzaExistsGuards],
     component: fromContainers.ProductItemComponent
   }
 ];
